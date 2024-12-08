d2472ea97b3037980f96aa9ac4f54ee57f3f5272
Juan Cruz <juancruzberrios73@gmail.com>
2024-12-07 23:55:15 -0300

    no anda coño

---
+++ springDemo/src/main/java/com/springDemo/Alien.java
@@ -0,0 +1,8 @@
+package com.springDemo;
+
+public class Alien {
+    public void talk(){
+        System.out.println("im an alien");
+    }
+    
+}
--- springDemo/src/main/java/com/springDemo/App.java
+++ springDemo/src/main/java/com/springDemo/App.java
@@ -1,5 +1,8 @@
 package com.springDemo;
 
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
 /**
  * Hello world!
  *
@@ -8,6 +11,9 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello World!" );
+        ApplicationContext context = new ClassPathXmlApplicationContext();
+        
+        Alien alien = (Alien)context.getBean("alien");
+        alien.talk();
     }
 }
