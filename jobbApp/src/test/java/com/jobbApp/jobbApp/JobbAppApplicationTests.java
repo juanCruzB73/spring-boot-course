23be976449308672539dded63945fdca1e905a0f
juanB <juancruzberrios73@gmail.com>
2024-12-19 19:19:51 -0300

    jobAPP

---
+++ jobbApp/src/test/java/com/jobbApp/jobbApp/JobbAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jobbApp.jobbApp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JobbAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
