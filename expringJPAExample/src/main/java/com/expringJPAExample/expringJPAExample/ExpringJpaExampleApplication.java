dd0cb605ee748bb0d5318bd93a285b3b433ff73c
juanB <juancruzberrios73@gmail.com>
2024-12-24 00:07:36 -0300

    jpa introduction

---
+++ expringJPAExample/src/main/java/com/expringJPAExample/expringJPAExample/ExpringJpaExampleApplication.java
@@ -0,0 +1,13 @@
+package com.expringJPAExample.expringJPAExample;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExpringJpaExampleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ExpringJpaExampleApplication.class, args);
+	}
+
+}
