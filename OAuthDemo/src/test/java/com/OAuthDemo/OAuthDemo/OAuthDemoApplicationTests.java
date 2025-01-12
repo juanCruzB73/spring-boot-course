83b690dcc3a8504421f3067373467b2d2fb7d840
juanB <juancruzberrios73@gmail.com>
2025-01-11 22:58:02 -0300

    OAuth demo

---
+++ OAuthDemo/src/test/java/com/OAuthDemo/OAuthDemo/OAuthDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.OAuthDemo.OAuthDemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OAuthDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
