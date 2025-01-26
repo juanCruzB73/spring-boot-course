32f656817b0033fa5e389a6a9af1b81200002e1b
juanB <juancruzberrios73@gmail.com>
2025-01-26 00:45:38 -0300

    DOCKER DE MIERDAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA

---
+++ dokerRestDemo/src/main/java/com/springcourse/dokerRestDemo/DokerRestDemoApplication.java
@@ -0,0 +1,13 @@
+package com.springcourse.dokerRestDemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DokerRestDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DokerRestDemoApplication.class, args);
+	}
+
+}
---
+++ dokerRestDemo/src/test/java/com/springcourse/dokerRestDemo/DokerRestDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.springcourse.dokerRestDemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DokerRestDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
